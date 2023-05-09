18e03cd1ebe28098646f7bab95dfb2635ab0a5bf
Cody Travis <chill@chillaspect.com>
2023-05-09 12:38:20 -0500

    Initial Commit
    Created all files and added README info.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# chillzam
+# ChillZam!
  Use the Shazam API to detect the song currently playing on a Twitch stream.
+
+ This tool uses [Streamlink](https://streamlink.github.io/) to record a few seconds of a specified Twitch stream, then it converts the audio to the format required by the [Shazam API](https://rapidapi.com/apidojo/api/shazam).
+
+ You will need to find out your Twitch GQL OAuth token. This token is different from the traditional Helix Twitch API token. If you need help finding the token check out this issue from the Streamlink github:
+ 
+  [How do I get a Twitch OAuth token to authenticate my requests?](https://github.com/streamlink/streamlink/discussions/4400)
+
+
+You will also need to generate a Shazam API key as well. Shazam has a free tier that allows 500 requests per month. Head over to the [Shazam Rapid API](https://rapidapi.com/apidojo/api/shazam) page and subscribe to it. You will automatically get a key.
+
+ChillZam uses FFmpeg to convert the audio to the Shazam format. If you have FFmpeg on your system already, just specify the path to it in the config file. If a path is not specified it will look in your PATH to see if its there. If its not in the config OR the PATH, it will attempt to use the binary in the `/bin` directory. This works for Windows and Mac. If you are on a Linux system you will need to install FFmpeg using your package manager.
+
+The script needs a config file to operate. This config file can be specified at the command link or just include a config.json file adjacent to the script. The config file has the following format:
+
+```json
+{
+    "twitch_channel": "https://www.twitch.tv/somestreamer",
+    "twitch_gql_oauth_token": "xxxxxxxxxxxxxxxxxxxxxxx",
+    "shazam_api_key": "xxxxxxxxxxxxxxxxxxxxxxx",
+    "ffmpeg_path": "",
+    "kbytes_to_record": 200
+}
+```
+
+The only field not mentioned yet is `kbytes_to_record`. 200KB is a good starting point. If its not identifying any songs, try to bump that number up. The max size that Shazam supports is 500kb.
+
+To use, clone the repository and run pip install on the requirements.txt file.
+
+```bash
+git clone https://github.com/chillfactor032/chillzam.git
+cd chillzam
+py -m pip install -r requirements.txt
+```
+
+Be sure to create the config file, then run the script
+
+```bash
+py chillzam.py
+```
+
+For Mac or Linux you may need to use `python` or `python3` instead of `py`. 
+
+Thanks!
+
+### Donations
+
+ChillZam is provided free and without warranty. If you feel compelled to donate here are my crypto addresses below.
+
+**Coin** | **Address**
+--- | ---
+BTC | 3C7UT1a2Do3LxFvxZt88S7gsNkRyRKXYCw
+ETH | 0xc24Fc5E6C2b3E1e1eaE62f59Fab8cFBC87b1FEfc
+LTC | MViPMqjn2kdMwbLAbYtgpgnHfzwwpbzUZQ
+
+### Contact
+
+chill@chillaspect.com
---
+++ chillzam.py
@@ -0,0 +1,171 @@
+import argparse
+import os
+import platform
+import sys
+import json
+import streamlink
+import requests
+import subprocess
+import base64
+from shutil import which
+
+system = platform.system()
+script_dir = os.path.realpath(os.path.dirname(__file__))
+config_path = os.path.join(script_dir, "config.json")
+config_src = "config.json in script dir"
+config = {}
+
+parser = argparse.ArgumentParser(description="Use the Shazam API to detect the current playing song on a Twitch stream.")
+parser.add_argument("-c", "--config", help="Path to config file. Check the README for an example")
+parser.add_argument("-v", "--verbose", action="store_true", help="Path to ffmpeg - used for converting audio.")
+args = parser.parse_args()
+
+def verbose_log(msg):
+    if args is not None and args.verbose:
+        print(msg)
+
+verbose_log("=== ChillZam! ===")
+verbose_log(f"Detected Platform: {system}")
+verbose_log(f"Script Dir: {script_dir}")
+verbose_log(f"Args: {args}")
+
+"""Find the config file"""
+if args.config:
+    if os.path.exists(args.config):
+        config_path = args.config
+        config_src = "CLI arg"
+    else:
+        sys.stderr.write("Specified config file does not exist.")
+        sys.exit(1)
+else:
+    if not os.path.exists(config_path):
+        sys.stderr.write("No config file could be found. Specify with -c option or a config.json in script dir.")
+        sys.exit(1)
+
+try:
+    with open(config_path) as config_file:
+        config = json.load(config_file)
+        verbose_log("Config file loaded.")
+except Exception as e:
+    sys.stderr.write("Could not load json config file. Improper json formatting?")
+    sys.exit(1)
+
+verbose_log(f"Config File: {config_path}")
+verbose_log(f"Config File Source: {config_src}")
+
+""" 
+If ffmpeg path is not specified in config,
+see if there is ffmpeg in PATH
+"""
+ffmpeg_source = "Config File"
+ffmpeg_path = config["ffmpeg_path"]
+if not os.path.exists(ffmpeg_path):
+    ffmpeg_path = which("ffmpeg")
+    if ffmpeg_path:
+        ffmpeg_source = "PATH"
+
+"""
+If ffmpeg not in PATH or specified
+use a versions from the
+repository (Windows or Mac)
+if linux - use package manager to install first
+"""
+if not ffmpeg_path:
+    ffmpeg_source = "Provided by repository"
+    if system == "Windows":
+        ffmpeg_path = os.path.join(script_dir, "bin", "win", "ffmpeg-win64.exe")
+    elif system == "Linux":
+        print("FFmpeg is not installed. Install it using your package manager.")
+        sys.exit(1)
+    elif system == "Darwin":
+        ffmpeg_path = os.path.join(script_dir, "bin", "mac", "ffmpeg")
+    else:
+        print(f"Unsupported OS Detected: {system}")
+        sys.exit(1)
+
+verbose_log(f"FFmpeg Source: {ffmpeg_source}")
+verbose_log(f"FFmpeg Path: {ffmpeg_path}")
+
+"""Confirm FFmpeg exists based on path"""
+if not os.path.exists(ffmpeg_path):
+    print("Could not find FFmpeg. Confirm it exists at path:")
+    print(ffmpeg_path)
+    sys.exit(1)
+else:
+    verbose_log("Confirmed FFmpeg exists.")
+
+#Song Data is Base64 Encoded
+def detectSong(raw_audio_b64, api_key):
+    url = "https://shazam.p.rapidapi.com/songs/v2/detect"
+    querystring = {"timezone":"America/Chicago","locale":"en-US"}
+    headers = {
+        "content-type": "text/plain",
+        "X-RapidAPI-Key": api_key,
+        "X-RapidAPI-Host": "shazam.p.rapidapi.com"
+    }
+    response = requests.post(url, data=raw_audio_b64, headers=headers, params=querystring)
+    return response.json()
+
+def convert_to_raw_audio(ffmpeg, in_file, out_file):
+    if os.path.exists(out_file):
+        os.remove(out_file)
+    if args.verbose:
+        subprocess.run([ffmpeg, '-i', in_file, "-vn", "-ar", "44100", "-ac", "1", "-c:a", "pcm_s16le", "-f", "s16le", out_file])
+    else:
+        subprocess.run([ffmpeg, '-i', in_file, "-vn", "-ar", "44100", "-ac", "1", "-c:a", "pcm_s16le", "-f", "s16le", out_file], 
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+    return os.path.exists(out_file)
+
+def record_stream(url, out_file, oauth_token, max_bytes=200):
+    #Remove out_file if it exists
+    if os.path.exists(out_file):
+        os.remove(out_file)
+    session = streamlink.Streamlink()
+    session.set_plugin_option("twitch", "api-header", [("Authorization", f"OAuth {oauth_token}")])
+    streams = session.streams(url)
+    stream_obj = streams["worst"]
+    verbose_log(f"Stream: {stream_obj}")
+    fd = stream_obj.open()
+    chunk = 1024
+    num_bytes = 0
+    data = b''
+    while num_bytes <= max_bytes*1024:
+        data += fd.read(chunk)
+        num_bytes+=chunk
+    fd.close()
+    with open(out_file, "wb") as file:
+        file.write(data)
+    return os.path.exists(out_file)
+
+""" Script Logic """
+verbose_log(f"Twitch URL: {config['twitch_channel']}")
+verbose_log(f"Begin recording stream ({config['bytes_to_record']} bytes)")
+stream_audio_file = "output.acc"
+if record_stream(config["twitch_channel"], stream_audio_file, config["twitch_gql_oauth_token"], config["kbytes_to_record"]):
+    verbose_log("Stream audio recorded successfully")
+else:
+    sys.stderr.write("Error recording stream audio.")
+    sys.exit(1)
+raw_audio_file = "output.raw"
+if convert_to_raw_audio(ffmpeg_path, stream_audio_file, raw_audio_file):
+    verbose_log("Audio converted successfully")
+else:
+    sys.stderr.write("Error converting stream audio from ACC to raw PCM s16le")
+    sys.exit(1)
+
+#Encode raw audio to base64
+with open(raw_audio_file, "rb") as song:
+    songBytes = song.read()
+    songb64 = base64.b64encode(songBytes)
+    l = len(songb64)
+    #Detect the song
+    matches = detectSong(songb64,config["shazam_api_key"])
+    if matches["track"]:
+        title = matches["track"]["title"]
+        artist = matches["track"]["subtitle"]
+        print(f"Song Detected: {title} by {artist}")
+        sys.exit(0)
+    else:
+        print("Could not detect song.")
+        sys.exit(0)
